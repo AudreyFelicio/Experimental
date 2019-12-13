cc363263032b2563a01777cbfb72c0b98e938e1c
RevolutionDz <audreyanwar@gmail.com>
2019-12-13 19:47:50 +0800

    Added the java script to make previous carousel alive

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React from 'react';
 import styled, {ThemeProvider} from 'styled-components';
 import Carousel from './Components/Carousel.js';
+//import Carousel from './Components/carouselNextVersion.js';
+import Image1 from './Pictures/picture1.jpeg';
+import Image2 from './Pictures/picture2.jpg';
+import Image3 from './Pictures/picture3.jpg';
 import './App.css';
 import Button1 from './Components/Button.js';
-import {theme1,theme2,theme3,theme4} from './Components/ButtonThemes.js'
+import {theme1,theme2,theme3,theme4} from './Components/ButtonThemes.js';
 
 
 function App() {
   return (
     <div className="app">
       <Carousel />
-      <div className="buttons-container">
-        <ThemeProvider theme={theme1}>
-          <div className="App">
-            <Button1>Theme One!</Button1>
-          </div>
-        </ThemeProvider>
-        <ThemeProvider theme={theme2}>
-          <div className="App">
-            <Button1>Theme Two!</Button1>
-          </div>
-        </ThemeProvider>
-        <ThemeProvider theme={theme3}>
-          <div className="App">
-            <Button1>Theme Two!</Button1>
-          </div>
-        </ThemeProvider>
-        <ThemeProvider theme={theme4}>
-          <div className="App">
-            <Button1>Theme Two!</Button1>
-          </div>
-        </ThemeProvider>
-      </div>
     </div>
   );
 }
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -8,17 +8,109 @@ import '../CSS/Carousel.css';
 
 
 class Carousel extends Component {
+
+    componentDidMount() {
+        const track = document.querySelector('.carousel__track');
+        const slides = Array.from(track.children);
+        const nextButton = document.querySelector('.carousel__button--right');
+        const prevButton = document.querySelector('.carousel__button--left');
+        const dotsNav = document.querySelector('.carousel__nav');
+        const dots = Array.from(dotsNav.children);
+
+        const slideSize = slides[0].getBoundingClientRect();
+        const slideWidth = slideSize.width;
+
+        //arrange the slides next to each other
+        const setSlidePosition = (slide,index) => {
+            slide.style.left = slideWidth * index + 'px';
+        };
+        slides.forEach(setSlidePosition);
+
+        const moveToSlide = (track, currentSlide, targetSlide) => {
+            track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+            currentSlide.classList.remove('current-slide');
+            targetSlide.classList.add('current-slide');
+        };
+
+        const updateDots = (currentDot, targetDot) => {
+            currentDot.classList.remove('current-slide');
+            targetDot.classList.add('current-slide');
+        }
+
+        const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+            if(targetIndex === 0) {
+                prevButton.classList.add('is-hidden');
+                nextButton.classList.remove('is-hidden');
+            } else if (targetIndex === slides.length-1) {
+                prevButton.classList.remove('is-hidden');
+                nextButton.classList.add('is-hidden');
+            } else {
+                prevButton.classList.remove('is-hidden');
+                nextButton.classList.remove('is-hidden');
+            }
+        }
+        //when click right, move slides to the right
+        nextButton.addEventListener('click', e =>{
+            const currentSlide = track.querySelector('.current-slide');
+            const nextSlide = currentSlide.nextElementSibling;
+            const currentDot = dotsNav.querySelector('.current-slide');
+            const nextDot = currentDot.nextElementSibling;
+            const nextIndex = slides.findIndex(slide => slide === nextSlide);
+            //move to next slide
+            moveToSlide(track, currentSlide, nextSlide);
+            //move dot
+            updateDots(currentDot, nextDot);
+
+            hideShowArrows(slides, prevButton, nextButton, nextIndex);
+        });
+
+        //when clickleft, move slides to the left
+        prevButton.addEventListener('click', e =>{
+            const currentSlide = track.querySelector('.current-slide');
+            const prevSlide = currentSlide.previousElementSibling;
+            const currentDot = dotsNav.querySelector('.current-slide');
+            const prevDot = currentDot.previousElementSibling;
+            const prevIndex = slides.findIndex(slide => slide === prevSlide);
+            //move to previous slide
+            moveToSlide(track, currentSlide, prevSlide);
+            //move dot
+            updateDots(currentDot, prevDot);
+
+            hideShowArrows(slides, prevButton, nextButton, prevIndex);
+        });
+        
+        //when click small navigator circle, move to that slide
+        dotsNav.addEventListener('click', e => {
+            //what was clicked?
+            const targetDot = e.target.closest('button');
+
+            const currentSlide = track.querySelector('.current-slide');
+            const currentDot = dotsNav.querySelector('.current-slide');
+            const targetIndex = dots.findIndex(dot => dot === targetDot);
+            const targetSlide = slides[targetIndex];
+            
+            if(targetDot !== null) {
+                //move to target slide
+                moveToSlide(track, currentSlide, targetSlide);
+                //move dot
+                updateDots(currentDot, targetDot);
+                
+                hideShowArrows(slides, prevButton, nextButton, targetIndex);
+            }
+        })
+    }
+
     render() {
         return (
             <div className="carousel">
 
-                <button className="carousel__button carousel__button--left">
+                <button className="carousel__button carousel__button--left is-hidden">
                     <img src={left} alt=""/>
                 </button>
 
                 <div className="carousel__track-container">
                     <ul className="carousel__track">
-                        <li className="carousel__slide">
+                        <li className="carousel__slide current-slide">
                             <img className="carousel__image" src={Image1} alt=""/>
                         </li>
                         <li className="carousel__slide">
